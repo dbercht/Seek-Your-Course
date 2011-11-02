b67a2a1655e207b5cfd7f16fd0a5b649a2c1893a
dbercht <d.bercht@gmail.com>
2011-11-02 20:42:40 -0300

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -27,9 +27,9 @@ SeekYourCourse::Application.routes.draw do
   resources :user_sessions
   resources :users
 
+  match 'ads' => 'ads#create', :via => :post
   match "ads" => "static#ads", :as => 'ads_index'
 	resources :ads, :except => [:show]
-  match 'ads' => 'ads#create', :via => :post
   match 'ads/:id' => "ads#show"
 
 
