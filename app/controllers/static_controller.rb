37247b3740eed5f38d8cfa2859ce3f6ce0ee430e
dbercht <d.bercht@gmail.com>
2011-11-18 21:04:24 -0500

    Update app/controllers/static_controller.rb

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,13 @@
 class StaticController < ApplicationController
   def home
-		@featured = Offering.where(:featured => true).first
-		@potential = Offering.find(:all, :order => "date(featured_date) DESC").first
-		if !@potential.nil?
-			@potential.update_attribute(:featured, true) unless @potential.nil?
-			@featured.update_attribute(:featured, false) unless @featured.nil?
-			@featured = @potential
-		end
+		#@featured = Offering.where(:featured => true).first
+		@featured = Offering.first :conditions => ["featured_date >= ?",Date.today], :order => "featured_date"
+		
+		#if !@potential.nil?
+	#		@potential.update_attribute(:featured, true) unless @potential.nil?
+#			@featured.update_attribute(:featured, false) unless @featured.nil?
+#			@featured = @potential
+#		end
     render :layout => false
   end
 
