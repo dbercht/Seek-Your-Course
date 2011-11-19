d55035332c8d4ea1de576e4691d919fb4ede16c1
dbercht <d.bercht@gmail.com>
2011-11-18 20:58:03 -0500

    Update app/controllers/static_controller.rb

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def home
 		@featured = Offering.where(:featured => true).first
-		@potential = Offering.find(:all, :order => "featured_date DESC").first
+		@potential = Offering.find(:all, :order => "date(featured_date) DESC").first
 		if !@potential.nil?
 			@potential.update_attribute(:featured, true) unless @potential.nil?
 			@featured.update_attribute(:featured, false) unless @featured.nil?
