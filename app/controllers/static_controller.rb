3b19de3b2ec3cce919784546bcbacb59b531f34e
dbercht <d.bercht@gmail.com>
2011-11-18 21:34:59 -0500

    Update app/controllers/static_controller.rb

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,8 @@ class StaticController < ApplicationController
   def home
 		#@featured = Offering.where(:featured => true).first
 	#	@featured = Offering.where(" date( featured_date ) <= date( now( ) ) ORDER BY featured_date DESC LIMIT 1")
-	@featured = Offering.where(" date( featured_date ) <= date( now( ) )").order("date(featured_date) DESC").first
+	#	@featured = Offering.where(" date( featured_date ) <= date( now( ) )").order("date(featured_date) DESC").first
+	@featured = Offering.find(:all, :conditions => ["date(featured_date) < DATE(NOW())"].order('featured_date DESC').first
 
 		#if !@potential.nil?
 	#		@potential.update_attribute(:featured, true) unless @potential.nil?
